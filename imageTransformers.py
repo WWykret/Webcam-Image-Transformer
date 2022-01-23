48a51d52df4290459799223a170fb30e72077a7a
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-01-23 16:35:11 +0100

    Finished working on transormations

--- imageTransformers.py
+++ imageTransformers.py
@@ -4,18 +4,34 @@ class ImageTransformer:
     def __init__(self):
         self.transformations = [IdentityTransformation()]
 
+    def tansform_image(self, image: Image):
+        for index, transformation in enumerate(self.transformations):
+            if index + 1 < len(self.transformations):
+                transformation.set_next(self.transformations[index + 1])
+
+        return self.transformations[0].transform_image(image)
+
+
 class ImageTransformerBuilder:
     def __init__(self):
         self.image_transformer = ImageTransformer()
 
     def negate_image(self):
         self.image_transformer.transformations.append(NegateTransformation())
+        return self
 
     def detect_edges(self):
         self.image_transformer.transformations.append(EdgeDetectionTransformation())
+        return self
 
-    def flip_image(self, axis: str):
+    def flip_on_axis(self, axis: str):
         self.image_transformer.transformations.append(FlipTransformation(axis))
+        return self
+
+    def flip_on_middle(self):
+        self.flip_on_axis('x')
+        self.flip_on_axis('y')
+        return self
 
     def build(self):
         return self.image_transformer
--- transformations.py
+++ transformations.py
@@ -1,24 +1,62 @@
-from PIL import Image
+from PIL import Image, ImageOps, ImageFilter
 
-class Transformation():
+class Transformation:
     def __init__(self):
         self.next = None
 
-    def set_next(self, next: Transformation):
-        pass
+    def set_next(self, next):
+        self.next = next
 
     def transform_image(self, image: Image):
         pass
 
 class FlipTransformation(Transformation):
     def __init__(self, flip_axis: str):
-        pass
+        super().__init__()
+        self.flip_axis = flip_axis
+
+    def transform_image(self, image: Image):
+        if self.flip_axis in ['x', 'X']:
+            image = ImageOps.flip(image)
+        elif self.flip_axis in ['y', 'Y']:
+            image = ImageOps.mirror(image)
+
+        if self.next is None:
+            return image
+        
+        return self.next.transform_image(image)
 
 class NegateTransformation(Transformation):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def transform_image(self, image: Image):
+        image = ImageOps.invert(image)
+
+        if self.next is None:
+            return image
+        
+        return self.next.transform_image(image)
 
 class EdgeDetectionTransformation(Transformation):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def transform_image(self, image: Image):
+        image = image.convert('L') # convert to grayscale
+        image = image.filter(ImageFilter.FIND_EDGES)
+
+        if self.next is None:
+            return image
+        
+        return self.next.transform_image(image)
 
 class IdentityTransformation(Transformation):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def transform_image(self, image: Image):
+        if self.next is None:
+            return image
+        
+        return self.next.transform_image(image)
